2f3f12178c5cc5d2301caf269f4c2934cf74c0c4
Aron <aronwc@gmail.com>
2017-08-23 13:30:52 -0500

    update

--- a0/a0.py
+++ a0/a0.py
@@ -5,7 +5,7 @@ CS579: Assignment 0
 Collecting a political social network
 
 In this assignment, I've given you a list of Twitter accounts of 4
-U.S. presedential candidates.
+U.S. presedential candidates from the previous election.
 
 The goal is to use the Twitter API to construct a social network of these
 accounts. We will then use the [networkx](http://networkx.github.io/) library
