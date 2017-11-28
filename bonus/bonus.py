a12fa6ddc309dc363357124eb2739818a084e482
Aron <aronwc@gmail.com>
2017-11-27 20:02:54 -0600

    bonus

---
+++ bonus/bonus.py
@@ -0,0 +1,15 @@
+import networkx as nx
+
+
+def jaccard_wt(graph, node):
+  """
+  The weighted jaccard score, defined above.
+  Args:
+    graph....a networkx graph
+    node.....a node to score potential new edges for.
+  Returns:
+    A list of ((node, ni), score) tuples, representing the 
+              score assigned to edge (node, ni)
+              (note the edge order)
+  """
+  pass
